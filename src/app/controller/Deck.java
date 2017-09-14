8e56c698e1efb4bf654a2d896996e0a138d9791f
PS <pg.zap34@gmail.com>
2017-09-15 00:10:31 +0200

    second commit

---
+++ src/app/controller/Deck.java
@@ -0,0 +1,7 @@
+package app.controller;
+
+public class Deck {
+	
+	private Card[] cards = 
+	
+}
---
+++ src/app/controller/Hand.java
@@ -0,0 +1,22 @@
+package app.controller;
+
+import javafx.scene.Parent;
+
+public class Hand  {
+	
+	private int aces = 0;
+
+	public void handCard (Cards cards) {
+	/*	
+		cards.add(card);
+		
+		if (wartosc wzietych + wartosc kart > 21 && aces > 0) {
+			ustawic wartosc (wartosc wzietych + wartosc kart 11 - 10);
+			aces --;
+		}
+		else {
+			ustawic wartosc (wartosc wzietych + wartosc kart );
+		}*/
+}
+	
+}
